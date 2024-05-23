1d2b2d90c213b8ce282abd24d2c27754dc4ede21
“Steven-Chidzanjagit config --global user.name “Steven-Chidzanja <bsc-ele-01-21@unima.ac.mw>
2024-05-23 14:13:55 +0200

    final tasks

---
+++ LabSession-9/main1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Person.h"
+using namespace std
+
+int main(){
+    Person Jane = Person("Jane", 60.0f);
+    Person John = Person("John", 75.0f);
+
+    float totalWeight = Jane + John;
+
+    cout << "Total weight: " << totalWeight << endl;
+
+    if (Jane == John){
+        cout << "This is the same person." << endl;
+    }
+    if (Jane != John){
+        cout << "This is NOT the same person." << endl;
+    }
+    if (Jane < John){
+        cout << "Jane is younger than John." << endl;
+    }
+    if (John > Jane){
+        cout << "John is older than Jane." << endl;
+    }
+    
+    return 0;
+}
---
+++ LabTask9/Triangle.h
@@ -0,0 +1,8 @@
+#include "Polygon.h"
+
+class Triangle : public Polygon 
+{
+    public:
+    int Area();
+
+};
